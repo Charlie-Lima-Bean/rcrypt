0c8398baf775403569342589cd6a1baeb731516a
cl <colinli.ext@pm.me>
2024-05-08 21:04:16 -0500

    crypttab formatting

--- init-crypt-data.sh
+++ init-crypt-data.sh
@@ -10,19 +10,21 @@ cryptname=$2
 keyname=$3
 mountpoint=$4
 
-if ! test -f /etc/rcrypt/$3; then
-  echo "/etc/rcrypt/$3" not found
+keypath=/etc/rcrypt/keys/$3
+
+if ! test -f $keypath; then
+  echo $keypath not found
   exit 1
 fi
 
 echo "setting up $destpart as $cryptname..."
 # will prompt for master passphrase
-cryptsetup -v -c aes-xts-plain64 -h sha512 -s 512 luksFormat $destpart /etc/rcrypt/$keyname --batch-mode
+cryptsetup -v -c aes-xts-plain64 -h sha512 -s 512 luksFormat $destpart $keypath --batch-mode
 echo "Input Backup Key..."
-cryptsetup luksAddKey $destpart --key-file /etc/rcrypt/keys/$keyname
+cryptsetup luksAddKey $destpart --key-file $keypath
 blkid | grep -i "$destpart"
-echo "$cryptname $(blkid -o value -s UUID $destpart) /etc/rcrypt/keys/$keyname discard" >> /etc/crypttab
-cryptsetup luksOpen --key-file /etc/rcrypt/keys/$keyname $destpart $cryptname
+echo "$cryptname UUID=$(blkid -o value -s UUID $destpart) $keypath discard" >> /etc/crypttab
+cryptsetup luksOpen --key-file $keypath $destpart $cryptname
 btrfs device add /dev/mapper/$cryptname $mountpoint
 
 # clevis luks regen -d $destpart -s 3
