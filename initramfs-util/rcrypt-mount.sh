a424408594e931e78574f0aa3b0c3b289496b5d8
cl <colinli.ext@pm.me>
2024-07-08 12:04:12 -0500

    missing read flag

--- initramfs-util/rcrypt-mount.sh
+++ initramfs-util/rcrypt-mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #backup script if the tpm gets shuffled
-read "passkey to use:" -s pw
+read -p "passkey to use:" -s pw
 while read -r devname uuid keyname ignore; do
     echo -n $pw | cryptsetup open /dev/disk/by-uuid/$uuid $devname --key-file=-
 done < /rcrypt/rtab.conf
